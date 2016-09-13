a87e4b846d2ef03a29d37515623bf690dad6fe8b
rmk1409 <rmk140987@gmail.com>
2016-09-13 15:28:21 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,32 @@
+import domain.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Created by r.pogorelov on 13.09.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        Configuration cfg = new Configuration();
+        cfg.configure();
+
+        SessionFactory factory = cfg.buildSessionFactory();
+        Session session = factory.openSession();
+        Transaction transaction = session.beginTransaction();
+
+        Employee e1 = new Employee();
+        e1.setId(1);
+        e1.setFirstName("rmk");
+        e1.setLastName("pogorelov");
+
+        session.persist(e1);
+
+        transaction.commit();
+        session.close();
+
+        System.out.println("Success!!");
+    }
+}
---
+++ src/main/java/domain/Employee.java
@@ -0,0 +1,33 @@
+package domain;
+
+/**
+ * Created by r.pogorelov on 13.09.2016.
+ */
+public class Employee {
+    private int id;
+    private String firstName, lastName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
